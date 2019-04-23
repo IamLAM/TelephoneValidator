15506e202c4801817f9dbae3bb4093e3da96b94b
IamLAM <ingeniero@luismonge.com.mx>
2019-04-23 14:12:33 -0700

    validation for (555)444-4444

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 function telephoneCheck(str) {
     // Good luck!
+    // let tel = [1, "(", 0, 0, 0, ")", "-", 0, 0, 0, "-", 0, 0, 0, 0];
+    let exp = /[\(]\d{3}[\)]\d{3}[\-]\d{4}/gi;
+    let val = exp.test(str);
+    console.log(val);
+}
 
-    regex=//;
-    
+//total numbers 10 obligatory
+//one number optional
+//two parenthesis optional
+//hyphen optional
+// dont matter spaces
 
-    return true;
-  }
-
-  //total numbers 10 obligatory
-  //one number optional
-  //two parenthesis optional
-  //hyphen optional
-  
-  telephoneCheck("555-555-5555");
+telephoneCheck("(665)444-0000");
