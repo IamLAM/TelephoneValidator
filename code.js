4d75ccf77d6a17346a7115b499f6566b4be02394
IamLAM <ingeniero@luismonge.com.mx>
2019-04-23 00:35:08 -0700

    function declared

---
+++ code.js
@@ -0,0 +1,15 @@
+function telephoneCheck(str) {
+    // Good luck!
+
+    regex=//;
+    
+
+    return true;
+  }
+
+  //total numbers 10 obligatory
+  //one number optional
+  //two parenthesis optional
+  //hyphen optional
+  
+  telephoneCheck("555-555-5555");
