a1ce27eb0dd2e6da565569d8337dfe7147586b16
IamLAM <ingeniero@luismonge.com.mx>
2019-04-24 03:11:50 -0700

    another type of solution

--- code.js
+++ code.js
@@ -1,15 +1,24 @@
 function telephoneCheck(str) {
     // Good luck!
-    // let tel = [1, "(", 0, 0, 0, ")", "-", 0, 0, 0, "-", 0, 0, 0, 0];
-    let exp = /[\(]\d{3}[\)]\d{3}[\-]\d{4}/gi;
+    //total numbers 10 obligatory
+    //one number optional
+    //two parenthesis optional
+    //hyphen optional
+    // dont matter spaces
+    let exp = /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/gi;
     let val = exp.test(str);
-    console.log(val);
+    if (val == true) {
+        console.log("telephone is valid");
+        return true;
+    } else {
+        console.log("telephone is invalid");
+        return false;
+    }
+
+
+
 }
 
-//total numbers 10 obligatory
-//one number optional
-//two parenthesis optional
-//hyphen optional
-// dont matter spaces
 
-telephoneCheck("(665)444-0000");
+
+telephoneCheck("555-5555");
